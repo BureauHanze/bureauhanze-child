f810ad151c84f432cf42140252e92d973aaef0c9
BureauHanze <93707852+BureauHanze@users.noreply.github.com>
2022-08-01 12:04:28 +0200

    Re-style Woocommerce E-mails

--- bureauhanze-child/woocommerce/emails/email-styles.php
+++ bureauhanze-child/woocommerce/emails/email-styles.php
@@ -43,6 +43,7 @@ $text_lighter_40 = wc_hex_lighter( $text, 40 );
 // !important; is a gmail hack to prevent styles being stripped if it doesn't like something.
 // body{padding: 0;} ensures proper scale/positioning of the email in the iOS native email app.
 ?>
+
 body {
 	padding: 0;
 }
@@ -50,16 +51,13 @@ body {
 #wrapper {
 	background-color: <?php echo esc_attr( $bg ); ?>;
 	margin: 0;
-	padding: 70px 0;
 	-webkit-text-size-adjust: none !important;
 	width: 100%;
 }
 
 #template_container {
-	box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1) !important;
-	background-color: <?php echo esc_attr( $body ); ?>;
-	border: 1px solid <?php echo esc_attr( $bg_darker_10 ); ?>;
-	border-radius: 3px !important;
+	box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1) !important;
+	background-color: <?php echo esc_attr( $body ); ?>;	
 }
 
 #template_header {
@@ -108,7 +106,12 @@ body {
 }
 
 #body_content table td {
-	padding: 48px 48px 32px;
+	padding-top:30px;
+	padding-bottom:30px;
+}
+
+#body_content table td img {
+	float:left;
 }
 
 #body_content table td td {
@@ -142,22 +145,143 @@ body {
 #body_content_inner {
 	color: <?php echo esc_attr( $text_lighter_20 ); ?>;
 	font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
-	font-size: 14px;
+	font-size: 17px;
 	line-height: 150%;
 	text-align: <?php echo is_rtl() ? 'right' : 'left'; ?>;
 }
 
 .td {
 	color: <?php echo esc_attr( $text_lighter_20 ); ?>;
-	border: 1px solid <?php echo esc_attr( $body_darker_10 ); ?>;
-	vertical-align: middle;
+	border: none;
 }
 
-.address {
-	padding: 12px;
-	color: <?php echo esc_attr( $text_lighter_20 ); ?>;
-	border: 1px solid <?php echo esc_attr( $body_darker_10 ); ?>;
+table {
+	border: none;
+	font-size: 17px;
 }
+
+#order_table {
+    border: none;
+	background: #f2f2f2;
+}
+
+#order_table table > tfoot > tr  {
+	background: <?php echo esc_attr($base);?>;
+}
+
+#order_table table > tfoot > tr  th, #order_table table > tfoot > tr  td {
+	color:white;
+}
+
+#order_table tfoot .woocommerce-Price-amount {
+	font-weight: 700;
+}
+
+#order_table table > tfoot > tr  th {
+	padding: 30px 0px 0px 30px;
+}
+
+#order_table table > tfoot > tr  td { 
+	padding: 30px 30px 0px 0px;
+}
+
+#order_table table > tfoot > tr:nth-child(2) th {
+	padding: 20px 0px 30px 30px;
+}
+
+#order_table table > tfoot > tr:nth-child(2) td { 
+	padding: 20px 30px 30px 0px;
+}
+
+#order_table table > tfoot > tr:nth-child(3) th {
+	padding: 30px 0px 30px 30px;
+	background-color: rgba(0, 0, 0, 0.1);
+}
+
+#order_table table > tfoot > tr:nth-child(3) td { 
+	padding: 30px 30px 30px 0px;
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+#order_table table > tfoot > tr:nth-child(4) th {
+	padding:30px 0px 0px 0px;
+	background-color: #ffffff;
+	color: <?php echo esc_attr( $text ); ?>;
+}
+
+#order_table table > tfoot > tr:nth-child(4) td { 
+	padding:30px 0px 0px 0px;
+	background-color: #ffffff;
+	color: <?php echo esc_attr( $text ); ?>;
+}
+
+address.address {
+	font-style: normal;
+	line-height: 1.4;
+}
+
+#order_table table > thead > tr th:first-child  {
+	padding: 15px 0px 15px 30px;
+}
+
+#order_table table > thead > tr th  {
+	padding: 15px 0px 15px 0px;
+}
+
+#order_table table > thead > tr th:last-child {
+	padding: 15px 30px 15px 12px;
+}
+
+#order_table table > thead > tr {
+	background: #d9d9d9;
+}
+
+#order_table table > tbody > tr td:first-child {
+	padding: 20px 0px 15px 30px;
+}
+
+#order_table table > tbody > tr > td {
+	padding: 20px 0px 20px 12px;
+	vertical-align: top !important;
+}
+
+#order_table table > tbody > tr.component_container_table_item td:first-child {
+	padding: 20px 0px 0px 30px;
+}
+
+#order_table table > tbody > tr.component_container_table_item > td {
+	padding: 20px 0px 0px 12px;
+	vertical-align: top !important;
+}
+
+#order_table table > thead > tr > th:first-child {
+	min-width: 310px;
+}
+
+#order_table table > thead > tr > th:nth-child(2) {
+	min-width: 70px;
+}
+
+#order_table table > tbody  tr:last-child > td {
+	padding: 12px 0px 30px 12px;
+}
+
+#order_table table > tbody  tr:last-child > td:nth-child(1) {
+	padding: 20px 0px 30px 30px;
+}
+
+
+#order_table table > tbody .component_table_item img {
+	display:none;
+}
+
+
+.includes_tax {
+	display:block;
+	font-size:13px;
+	padding-left:4px;
+}
+
 
 .text {
 	color: <?php echo esc_attr( $text ); ?>;
@@ -168,27 +292,49 @@ body {
 	color: <?php echo esc_attr( $link_color ); ?>;
 }
 
-#header_wrapper {
-	padding: 36px 48px;
-	display: block;
+#header_logo_wrapper {
+	padding: 14px 0px;
+}
+
+#header_logo_wrapper img {
+	max-width:100%;
+	height:55px;
+	width:auto;
+    display: block;
+}
+
+#main_title {
+	background: #f2f2f2;
+    padding: 30px;
+    line-height: 1;
+	margin-bottom:16px;
+}
+
+#main_title h1, #main_title h2 {
+	margin:0px;
+	line-height:1;
+}
+
+#main_title h1 {
+	margin-bottom: 10px;
 }
 
 h1 {
 	color: <?php echo esc_attr( $base ); ?>;
 	font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
 	font-size: 30px;
-	font-weight: 300;
+	font-weight: 600;
 	line-height: 150%;
-	margin: 0;
+	margin-bottom: 20px;
+	margin-top: 0px;
 	text-align: <?php echo is_rtl() ? 'right' : 'left'; ?>;
-	text-shadow: 0 1px 0 <?php echo esc_attr( $base_lighter_20 ); ?>;
 }
 
 h2 {
 	color: <?php echo esc_attr( $base ); ?>;
 	display: block;
 	font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
-	font-size: 18px;
+	font-size: 22px;
 	font-weight: bold;
 	line-height: 130%;
 	margin: 0 0 18px;
@@ -212,10 +358,9 @@ a {
 	text-decoration: underline;
 }
 
-img {
+#body_content img {
 	border: none;
 	display: inline-block;
-	font-size: 14px;
 	font-weight: bold;
 	height: auto;
 	outline: none;
@@ -226,4 +371,32 @@ img {
 	max-width: 100%;
 	height: auto;
 }
+
+#body_content #order_details {
+	margin-top: 30px;
+}
+
+blockquote {
+	background: #f2f2f2;
+    width: 600px;
+    margin: 0px;
+    padding: 20px;
+    margin-bottom: 16px;
+    border: solid 1px #3333;
+}
+
+#footer_left, #footer_right {
+	font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
+	font-size: 13px;
+	color: #333333;
+}
+
+#footer_right {
+	text-align:right;
+}
+
+#template_footer {
+	padding: 25px 0px;
+}
+
 <?php
